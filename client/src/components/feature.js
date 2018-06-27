02f33198ef8a14ae9defd4f52cbfcbbd6ea7afae
Allan.W.C <allan_195@hotmail.com>
2018-06-27 14:44:33 +1000

    commit before recommencing work

--- client/src/components/feature.js
+++ client/src/components/feature.js
@@ -1,19 +1,42 @@
+// import React, { Component } from 'react';
+
+
+// class Feature extends Component {
+//     componentWillMount() {
+//         this.props.fetchMessage();
+//     }
+//     render() {
+//         return (
+//             <div className="chat-screen">
+//                 <ul id="messages"></ul>
+//                 <form action="">
+//                 <input id="m" autocomplete="off" />
+//                 <button>Send</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-
+ 
 class Feature extends Component {
-    componentWillMount() {
-        this.props.fetchMessage();
+    constructor(props, context) {
+        super(props, context);
+        this.onMessage = this.onMessage.bind(this);
     }
+ 
+    onMessage(message) {
+        console.log(message);
+    }
+ 
     render() {
         return (
-            <div className="chat-screen">
-                <ul id="messages"></ul>
-                <form action="">
-                <input id="m" autocomplete="off" />
-                <button>Send</button>
-                </form>
+            <div>
+                <h1>My React SocketIO Demo.</h1>
+                <Event event='eventName' handler={this.onMessage} />
             </div>
         );
     }
