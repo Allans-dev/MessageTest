f8d284480bcee0848ba4ef07a181ce087857359f
Allan.W.C <allan_195@hotmail.com>
2018-09-23 17:12:20 +1000

    added skeleton form to feature refactor messageDisplay

--- client/src/components/feature.jsx
+++ client/src/components/feature.jsx
@@ -2,17 +2,54 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-import MessageDisplay from './messageDisplay';
+// import MessageDisplay from './messageDisplay';
 
 class Feature extends Component {
+    constructor(props){
+        super(props);
+        this.state = { message: '' }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
     componentWillMount() {
         this.props.fetchMessage();
     }
+
+    handleChange(event) {
+        this.setState({message: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        event.preventDefault();
+
+            // convert jquery to react
+            // onSubmit use socket.io to pop to another area of the page on all users
+            
+            // $(function () {
+            //     const socket = io();
+            //      $('form').submit(function(){
+            //          socket.emit('chat message', $('#m').val());
+            //          $('#m').val('');
+            //          return false;
+            //      });
+            //      socket.on('chat message', function(msg){
+            //          $('#messages').append($('<li>').text(msg));
+            //      });
+            //  });
+
+      }
+
     render() {
         return (
             <div>
                 <div>{this.props.message}</div>
-                <MessageDisplay />  
+                <form>
+                    <label>
+                        <input type="text" name="message" value={this.state.message} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="submit" onSubmit={this.handleSubmit} />
+                </form> 
             </div>
         );
     }
--- client/src/components/messageDisplay.jsx
+++ client/src/components/messageDisplay.jsx
@@ -1,3 +1,3 @@
-import React from 'react';
+// import React from 'react';
 
-export default (props) => <div>Message from around the world</div>
+// export default (props) => <div>Message from around the world</div>
