952e1ffdd0bc54bdbbcb5ae3f8075192cdfefb49
Allan.W.C <allan_195@hotmail.com>
2018-11-13 06:52:09 +1100

    updated process.env

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const router = require('./router');
-// const configdb = require('./config');
 
 // DB setup
-const dbUrl = configdb.database;
-mongoose.connect(process.env.MONGODB_URI || dbUrl);
+const dbUrl = 'mongodb://localhost:27017/messageTest'
+mongoose.connect(process.env.database || dbUrl);
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const User = require('../models/user');
-// const config = require('../config');
+const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
@@ -27,7 +27,7 @@ const localLogin = new LocalStrategy(localOptions, function(email, password, don
 // setup option for JWT strategy
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-    secretOrKey: process.env.secret
+    secretOrKey: process.env.secret || config.secret
 };
 
 // create JWT strategy
