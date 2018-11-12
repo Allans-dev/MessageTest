3688f1df93ffe5e710fcc5dabcdf325479c213f5
Allan.W.C <allan_195@hotmail.com>
2018-11-12 19:20:18 +1100

    updated sign-ins to allow access to same room

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,6 @@ import openSocket from 'socket.io-client';
 const socket = openSocket("http://localhost:3090");
 
 function subscribeToTimer(cb) {
-    socket.on('connection', console.log(timestamp => cb(null, timestamp)));
     socket.on('timer', timestamp => cb(null, timestamp));
     socket.emit('subscribeToTimer', 1000);
 } 
--- server/socket-events.js
+++ server/socket-events.js
@@ -1,28 +1,28 @@
 
 module.exports = (io) => {
 
-    io.on('connection', (client) => {
+    io.on('connection', (socket) => {
 
       console.log('socket.io is connected');
 
-      client.on('disconnect', function(){
+      io.on('disconnect', function(){
           console.log('user disconnected');
       });
 
-      client.on('subscribeToTimer', (interval) => {
+      socket.on('subscribeToTimer', (interval) => {
         console.log('client is subscribing to timer with interval ', interval);
         setInterval(() => {
-          client.emit('timer', new Date());
+          socket.emit('timer', new Date());
         }, interval);
       });
 
-      client.on('chat message', function(msg){
-          client.emit('chat message', msg);
+      socket.on('chat message', function(msg){
+          io.emit('chat message', msg);
           console.log('message: ' + msg);
       });
 
-      client.on('end', function (){
-        client.disconnect(0);
+      socket.on('end', function (){
+        socket.disconnect(0);
        });
 
     });
